4c03d521a6c1082eefd834d795a0b5655ad253da
sabzyhacks <ei711283@gmail.com>
2026-07-23 14:54:02 +0100

    get remaining time implemented

--- script.js
+++ script.js
@@ -11,9 +11,21 @@ const resetBtn = document.querySelector("#resetBtn");
 
 const now = new Date();
 
+const calcRemainingTime = function (target, now) {
+  return target.getTime() - now.getTime();
+};
+const getRemainingDate = function (milliSec) {
+  return {
+    day: Math.floor(milliSec / (1000 * 60 * 60 * 24)),
+    hour: Math.floor((milliSec / (1000 * 60 * 60)) % 24),
+    minute: Math.floor((milliSec / (1000 * 60)) % 60),
+    second: Math.floor((milliSec / 1000) % 60),
+  };
+};
+
 startBtn.addEventListener("click", function (e) {
   e.preventDefault();
   const targetDate = new Date(targetDateInput.value);
-  const timeLeft = targetDate.getTime() - now.getTime();
-  console.log(timeLeft);
+  const timeLeft = calcRemainingTime(targetDate, now);
+  console.log(getRemainingDate(timeLeft));
 });
