3fc9e9f8823af035b0d9a3c2a7ef9412fa2c0349
sabzyhacks <ei711283@gmail.com>
2026-07-23 12:41:38 +0100

    added javascript file, added needed variables

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+const daysHtmlEl = document.querySelector("#days");
+const hoursHtmlEl = document.querySelector("#hours");
+const minutesHtmlEl = document.querySelector("#minutes");
+const secondsHtmlEl = document.querySelector("#seconds");
+
+const targetDateInput = document.querySelector("#targetDate");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resetBtn = document.querySelector("#resetBtn");
